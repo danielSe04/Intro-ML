1d0361d45a3d4fe63b44955a1e53c4d6fbbb560a
danielSe04 <dan.seidel.2004@gmx.de>
2025-02-16 18:38:08 +0100

    Wuhu duck

--- a1.py
+++ a1.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import sys
 import numpy as np
 import scipy
-
+import sklearn
 
 
 def normalize_data(x):
@@ -19,12 +19,25 @@ def calculate_eigen(z):
     eigenvalues, eigenvectors = np.linalg.eig(cov)
     return eigenvalues, eigenvectors
 
-#def dimension_reduced_data(x,y,d,perplexity,random_state):
+def dimension_reduced_data(x, y, d, perplexity, random_state):
+    colors = ["blue", "orange", "green", "red", "purple", "brown", "pink", "grey", "olive", "cyan"]
+    _, _, z = pca(x, d)
+    tsne = sklearn.manifold.TSNE(n_components=2, random_state=random_state, perplexity=perplexity)
+    z_2d = tsne.fit_transform(z)
+    print(np.shape(z_2d))
+    plt.figure(figsize=(6.4,4.8), dpi=100)
+    for i, point in enumerate(z_2d):
+        plt.plot(point[0], point[1], marker = "o", color=colors[((int) (i / 72)) % len(colors)], label=f"Class {(int) (i / 72)}")
+    plt.legend(
+        title="Object ID",
+        bbox_to_anchor=(1.05, 1),
+        loc="upper left"
+    )
+    plt.tight_layout()
+    plt.savefig("plot.png")
 
 def eigen_value_profile(x, d):
     _, values, _ = pca(x, d)
-
-    x = range(1, len(values) + 1)
 
     fig, ax = plt.subplots(figsize=(6.4,4.8), dpi=100)
     ax.plot(x,values, color='purple')
@@ -56,3 +69,7 @@ def pca(x, d):
 
 dict_in = scipy.io.loadmat("COIL20.mat")
 x_coords = np.array(dict_in['X'])
+y_coords = np.array(dict_in['Y'])
+print(y_coords[73])
+print(np.shape(y_coords))
+dimension_reduced_data(x_coords, y_coords, 40, 4, 42)
