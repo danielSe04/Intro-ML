a2f509dada02fefbb6dfe1375ba913f0f25019e5
Erias6 <andrei.mihneapasere@gmail.com>
2025-02-18 17:51:52 +0100

    Added comments to the code

--- a1.py
+++ a1.py
@@ -4,14 +4,26 @@ import numpy as np
 import scipy
 import sklearn
 
+'''
+This file contains functions for dimensionality reduction and eigen-value profiling of the dataset.
+Authors: Daniel Seidel & Mihnea Pasere
+'''
 
+'''
+This function takes in one argument which is a numpy array and returns a normalized version of the array.
+The normalized version is calculated by subtracting the mean of the array from each element 
+and dividing each element by the standard deviation of the array.
+'''
 def normalize_data(x):
     if not isinstance(x, np.ndarray):
         x = np.array(x)
     mean = np.mean(x, axis=0)
     sd = np.std(x, axis=0)
     return (x-mean)/sd
-
+'''
+This function takes in as argument a normalized set of data, which is a numpy array.
+It then computes the covariance matrix of the data and returns the eigenvalues and eigenvectors of that matrix.
+'''
 def calculate_eigen(z):
     # Take co-variance matrix C of z
     cov = np.cov(z, rowvar=False)
@@ -19,8 +31,11 @@ def calculate_eigen(z):
     eigenvalues, eigenvectors = np.linalg.eig(cov)
     return eigenvalues, eigenvectors
 
+'''
+The function below realizes the t-SNE visualization of the dataset.
+It also reduces the data x to d dimensions using PCA.
+'''
 def dimension_reduced_data(x, y, d, perplexity, random_state):
-    #colors = ["teal", "gold", "lime", "indigo", "magenta", "yellow", "darkblue", "darkred", "lightblue", "lightgreen"]
     _, _, z = pca(x, d)
     tsne = sklearn.manifold.TSNE(n_components=2, random_state=random_state, perplexity=perplexity)
     z_2d = tsne.fit_transform(z)
@@ -35,6 +50,9 @@ def dimension_reduced_data(x, y, d, perplexity, random_state):
     plt.tight_layout()
     plt.savefig("Plot.png")
 
+'''
+This function outputs the distribution of the eigenvalues of the dataset.
+'''
 def eigen_value_profile(x, d):
     _, values, _ = pca(x, d)
 
@@ -46,12 +64,20 @@ def eigen_value_profile(x, d):
     ax.set_title("Eigen-value Profile of the Dataset")
     fig.savefig(sys.stdout.buffer)
 
+'''
+Takes the first image from the dataset in its array form and plots it as an image.
+'''
 def input_data_sample():
     plt.title("Input data sample as an image")
     plt.imshow(np.reshape(x_coords[0,:],(32,32)))
     plt.savefig(sys.stdout.buffer)
 
-# Output: Principal components, eigen-values, reduced version of data set
+'''
+The function is used to implement the standard PCA algorithm, reducing the dimension of x to d.
+This function outputs three things: Principal components, eigenvalues, reduced version of data set
+It uses previously defined functions to normalize the data, compute eigenvalues and eigenvectors, and then sort them.
+Finally, it reduces the dimensionality of the data by matrix multiplication with the first d eigenvectors.
+'''
 def pca(x, d):
     z = normalize_data(x)
     eigenvalues, eigenvectors = calculate_eigen(z)
@@ -64,7 +90,9 @@ def pca(x, d):
     reduced_z = z @ vectors
     return vectors, values, reduced_z
 
+'''
+Defines the input data.
+'''
 dict_in = scipy.io.loadmat("COIL20.mat")
 x_coords = np.array(dict_in['X'])
 y_coords = np.array(dict_in['Y'])
-dimension_reduced_data(x_coords, y_coords, 40, 4, 42)
