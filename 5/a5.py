d1f4b5e059d3b9eb4f350888e6905d7280d7e80e
danielSe04 <dan.seidel.2004@gmx.de>
2025-03-13 13:36:24 +0100

    Something else

--- 5/a5.py
+++ 5/a5.py
@@ -32,26 +32,24 @@ def linear_vector_quantization(num_prototypes, learning_rate, max_epoch):
     prototypes = []
     misclassifications = []
     errors = []
+    reshaped_data = data.reshape(2*CLASS_SIZE, -1)
     # Generate prototypes
     for i, data_class in enumerate(data):
         random_indices = np.random.choice(len(data_class), size=num_prototypes)
         for index in random_indices:
-            prototypes.append([data_class[index], i])
+            prototypes.append([data_class[index], i+1])
     prototype_trace = [[p[0] for p in prototypes]]
-    previous_labels = []
     for epoch in range(max_epoch):
         misclassifications_epoch = 0
-        labels = []
-        point_set = np.random.permutation(data.reshape(2*CLASS_SIZE, -1))
+        permutation = np.random.permutation(len(reshaped_data))
+        point_set = reshaped_data[permutation]
+        labels_permuted = data_labels[permutation]
         for i,point in enumerate(point_set):
             index, minimum = find_closest_prototype(prototypes, point)
-            sign = 1 if prototypes[index][1] == (int)(i / CLASS_SIZE) else -1
+            sign = 1 if prototypes[index][1] == labels_permuted[i] else -1
 
             if sign == -1: misclassifications_epoch += 1
-            if not isinstance(prototypes[index][0], np.ndarray):
-                print("error")
-            prototypes[index][0] += learning_rate * sign * (point-prototypes[index][0])
-            labels.append(prototypes[index][1])
+            prototypes[index][0] = prototypes[index][0] + learning_rate*sign*(point-prototypes[index][0])
         prototype_trace.append([[p[0][0], p[0][1]] for p in prototypes])
         misclassifications.append(misclassifications_epoch)
         # if error does not change anymore, return
@@ -63,13 +61,39 @@ def linear_vector_quantization(num_prototypes, learning_rate, max_epoch):
 
     return prototype_trace, predicted_labels, misclassifications
 
+def plot_error_rate(num_prototypes, learning_rate, max_epoch):
+    _, _, errors = linear_vector_quantization(num_prototypes, learning_rate, max_epoch)
+    plt.plot(range(len(errors)), errors/100)
+    plt.xlabel('Epoch')
+    plt.ylabel('The error rate in %')
+    plt.title('Learning curve')
+    plt.savefig(sys.stdout.buffer)
+    plt.close()
+
+def plot_trajectory(num_prototypes, learning_rate, max_epoch):
+    prototype_trace, _, _ = linear_vector_quantization(num_prototypes, learning_rate, max_epoch)
+    plt.figure()
+    plt.scatter(data[0][:, 0], data[0][:, 1], color='red')
+    plt.scatter(data[1][:, 0], data[1][:, 1], color='blue')
+    plt.scatter(prototype_trace[-num_prototypes, 0, 0], prototype_trace[-num_prototypes, 0, 1], color='red',marker='*',s=200)
+    plt.scatter(prototype_trace[-num_prototypes, 1, 0], prototype_trace[-num_prototypes, 1, 1], color='blue',marker='*',s=200)
+
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    plt.title('Trajectory Of Prototypes')
+    plt.savefig("plot.png")
+    plt.close()
+
 data = []
 with open("data_lvq.csv", "r") as f:
     csv_file = csv.reader(f)
     for line in csv_file:
         data.append([float(line[0]), float(line[1])])
 data = np.array([data[:CLASS_SIZE], data[CLASS_SIZE:2*CLASS_SIZE]])
+data_labels = np.array([(int) (i / CLASS_SIZE) + 1 for i in range(2*CLASS_SIZE)])
 prototypes, labels, misclassifications = linear_vector_quantization(1, 0.002, 100)
 
 print(prototypes)
 print(misclassifications)
+
+plot_trajectory(2, 0.002, 100)
